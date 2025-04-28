cfa612032ee617af290cf8b4a20ddf2f61631a0f
kurehasan <promomiji04mus914@gmail.com>
2025-04-28 11:46:20 +0900

    Initial Commit

---
+++ remind games/remind_gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  remind_gamesApp.swift
+//  remind games
+//
+//  Created by 浅沼紅葉 on 2025/04/28.
+//
+
+import SwiftUI
+
+@main
+struct remind_gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ remind gamesTests/remind_gamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  remind_gamesTests.swift
+//  remind gamesTests
+//
+//  Created by 浅沼紅葉 on 2025/04/28.
+//
+
+import Testing
+@testable import remind_games
+
+struct remind_gamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
